84e33d0a9c236f9185f735b2d8558a5224eb98db
Dipongkor-Roy <juniordipu@gmail.com>
2024-01-16 02:13:32 +0600

    home page working

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <>
+   <h2>Working</h2>
+   <button className="btn btn-lg">Large</button>
+<button className="btn">Normal</button>
+<button className="btn btn-sm">Small</button>
+<button className="btn btn-xs">Tiny</button>
+   </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+import Catagory from "../Category/Catagory";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Catagory/>
+        </div>
+    );
+};
+
+export default Home;
