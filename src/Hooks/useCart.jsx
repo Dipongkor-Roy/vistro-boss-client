c448170493e221843af1d40c4f8c2218d742ce89
Dipongkor-Roy <juniordipu@gmail.com>
2024-01-31 03:05:40 +0600

    usequeries fun setup done

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,9 +1,20 @@
-
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../Providers/AuthCont';
 
 const useCart = () => {
-    return (
-   <h2></h2>
-    );
+    const [user]=useContext(AuthContext);
+    const { refetch, data:cart=[]} = useQuery({
+        queryKey: ['cart',user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:2000/carts?email=${user.email}`)
+            return res.json()
+      },
+      
+    })
+    
+    return [cart,refetch]
+ 
 };
 
 export default useCart;
