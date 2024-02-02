d6ea929d6876490747ae22b1b50629a272448a4e
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-02 12:40:27 +0600

    dashboard sidebar coller added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FaCalendar, FaHamburger, FaHome, FaShoppingCart, FaWallet } from "react-icons/fa";
 
 
@@ -13,23 +13,23 @@ const Dashboard = () => {
         </div> 
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <ul className="menu p-4 w-80 min-h-full  bg-yellow-500 text-base-content">
             {/* Sidebar content here */}
-            <li><Link to=''><FaHome />User Home</Link></li>
-            <li><Link to=''><FaCalendar />Resevation</Link></li>
-            <li><Link to=''><FaWallet />Payments History</Link></li>
-            <li> <Link to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</Link></li>
+            <li><NavLink to='/dashboard/Home'><FaHome />User Home</NavLink></li>
+            <li><NavLink to='/dashboard/Reservation'><FaCalendar />Resevation</NavLink></li>
+            <li><NavLink to='/dashboard/History'><FaWallet />Payments History</NavLink></li>
+            <li> <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
             <div className="divider"></div>
-            <li><Link to='/'><FaHome />Home</Link></li>
+            <li><NavLink to='/'><FaHome />Home</NavLink></li>
             <li>
-        <Link to="/menu">
+        <NavLink to="/menu">
           <FaHamburger/>Menu
-        </Link>
+        </NavLink>
       </li>
       <li>
-        <Link to="/order">
+        <NavLink to="/order">
           <a>Order</a>
-        </Link>
+        </NavLink>
       </li>
           </ul>
         
--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -4,12 +4,12 @@ import useCart from "../../../Hooks/useCart";
 const MyCart = () => {
     const [cart]=useCart();
     const total=cart.reduce((sum,item)=>item.price+sum,0) //here item traverse and total amount store in sum
-    
+
     return (
-        <div>
-            <h2>Total Items: {cart.length}</h2>
-            <h2>Total Price: {cart.length}</h2>
-            <button className="btn btn-circle">Dj</button>
+        <div className="uppercase">
+            <h2 className="text-2xl">Total Items: {cart.length}</h2>
+            <h2 className="text-2xl">Total Price: {total}</h2>
+            <button className="btn btn-warning btn-circle">Pay</button>
         </div>
     );
 };
