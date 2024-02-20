96d53de525a78fede07ae66a8730b27d536d5e21
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-21 00:14:50 +0600

    solve the admin added error issue

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- const asioxSecure =axios.create({
-    baseURL:"http://localhost:2000/"
+ const asioxSecure = axios.create({
+    baseURL:'http://localhost:2000/',
  })
 const UseAxiosSecure = () => {
     return asioxSecure;
