9aff6a9b8125a7beeae1441175833a7fe327e0b2
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-22 02:22:43 +0600

    form setup start in add item section

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,10 +1,38 @@
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const AddItems = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <SectionTitle
+        heading="Added An Item"
+        subheading="Whats New ?"
+      ></SectionTitle>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} />
+          <select
+            {...register("category")}
+            className="select select-bordered w-full max-w-xs"
+          >
+            <option disabled selected>
+              Select a category
+            </option>
+            <option value="salad">Salad</option>
+            <option value="pizza">Pizza</option>
+            <option value="desert">Desert</option>
+            <option value="drinks">Drinks</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
